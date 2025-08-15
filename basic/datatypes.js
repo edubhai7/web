a929cd8c2b0a94d1ea3829f9f1300fec004297cc
edubhai7 <edutechverse@gmail.com>
2025-08-15 16:04:05 +0000

    container config done

---
+++ basic/datatypes.js
@@ -0,0 +1,7 @@
+'use strict';// treat all js code as newer version
+let name = 'vikas';
+let age = 34;
+let islogged = false;
+let state;
+console.table([name,age,islogged,state])
+console.log(typeof name)
